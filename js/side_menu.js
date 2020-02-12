98d488d660816fa9913c49a85ef63d1160c78c87
Aniket <aniketbhaisare1998@gmail.com>
2020-02-12 11:37:01 +0530

    dmbjs

---
+++ js/side_menu.js
@@ -0,0 +1,8 @@
+function side_menu_item(){
+    document.write("<p>item_1</p>");
+    document.write("<p>item_1</p>");
+    document.write("<p>item_1</p>");
+    document.write("<p>item_1</p>");
+    document.write("<p>item_1</p>");
+    document.write("<p>item_1</p>");
+}
